4250502232a99a72459482710aece80b79ee6cc3
Alexandr Parlakyan <alex_man2@rambler.ru>
2023-04-17 17:57:46 +0200

    Experiments

--- src/test/java/OrderScooterTestsFirefox.java
+++ src/test/java/OrderScooterTestsFirefox.java
@@ -1,117 +1,67 @@
-import baseTest.BaseSetUpFireFox;
+import base.BaseSetUpFirefox;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import page.ActionWithOrder;
 
-import static org.junit.Assert.assertEquals;
-
-public class OrderScooterTestsFirefox extends BaseSetUpFireFox {
+public class OrderScooterTestsFirefox extends BaseSetUpFirefox {
 
     @Test
     public void testOrderScooterUpButton() {
         //Нажимаем кнопку Заказать
-        WebElement orderButtonTop = driver.findElement(By.xpath(".//button[@class='Button_Button__ra12g']"));
-        orderButtonTop.click();
-        //Заполняем поля заказа
+        ActionWithOrder.clickButtonOrderUp();
+        // Заполняем поля заказа
         //Имя
-        WebElement nameInput = driver.findElement(By.xpath(".//input[@placeholder='* Имя']"));
-        nameInput.sendKeys("Александр");
+        ActionWithOrder.sendNameInput();
         //Фамилия
-        WebElement surnameInput = driver.findElement(By.xpath(".//input[@placeholder='* Фамилия']"));
-        surnameInput.sendKeys("Парлакян");
+        ActionWithOrder.sendSurnameInput();
         //Адресс доставки
-        WebElement addressInput = driver.findElement(By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']"));
-        addressInput.sendKeys("Ленинградский проспект дом 2 кор.1");
+        ActionWithOrder.sendAddressInput();
         //Телефон для связи с курьером
-        WebElement phoneInput = driver.findElement(By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']"));
-        phoneInput.sendKeys("+79211065265");
+        ActionWithOrder.sendPhoneInput();
         //Выбираем станцию метро Белорусская
-        WebElement stationMetro = driver.findElement(By.xpath(".//input[@placeholder='* Станция метро']"));
-        stationMetro.click();
-        stationMetro.sendKeys("Белорусская");
-        WebElement stationMetroUser = driver.findElement(By.xpath(".//button[@value='29']"));
-        stationMetroUser.click();
+        ActionWithOrder.choiceStationMetro();
         //Нажимаем кнопку далее
-        WebElement orderButtonNext = driver.findElement(By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"));
-        orderButtonNext.click();
+        ActionWithOrder.orderButtonNextPage();
         //Заполняем дату доставки
-        WebElement orderArrivalDate = driver.findElement(By.xpath(".//input[@placeholder='* Когда привезти самокат']"));
-        orderArrivalDate.click();
-        orderArrivalDate.sendKeys("10.10.2025");
-        orderArrivalDate.sendKeys(Keys.ENTER);
-        WebElement orderRentalPeriod = driver.findElement(By.className("Dropdown-control"));
-        orderRentalPeriod.click();
-        WebElement orderRentalPeriodDay = driver.findElement(By.className("Dropdown-option"));
-        orderRentalPeriodDay.click();
+        ActionWithOrder.deliveryDateSelection();
         //Выбираем чек-бокс с цветом самоката
-        WebElement colorRentScooter = driver.findElement(By.id("black"));
-        colorRentScooter.click();
+        ActionWithOrder.choiceColorRentScooter();
         //Заполняем комментарий
-        WebElement commentCourier = driver.findElement(By.xpath(".//input[@placeholder='Комментарий для курьера']"));
-        commentCourier.sendKeys("Надо срочно");
+        ActionWithOrder.sendCommentCourier();
         //Нажимаем кнопку Заказать
-        WebElement makeAnOrder = driver.findElement(By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"));
-        makeAnOrder.click();
+        ActionWithOrder.pressMakeAnOrder();
         //Нажимаем кнопку подтверждения заказа
-        WebElement confirmTheOrder = driver.findElement(By.xpath(".//button[contains(@class, 'Button_Button__ra12g') and text()='Да']"));
-        confirmTheOrder.click();
+        ActionWithOrder.pressConfirmTheOrder();
         //Выполняем сверку окна подтверждения заказа по тексту
-        WebElement orderNumberWindow = driver.findElement(By.cssSelector("div.Order_ModalHeader__3FDaJ"));
-        String orderNumberText = orderNumberWindow.getText();
-        assertEquals("Надпись 'Заказ оформлен' отображается корректно", true, orderNumberText.contains("Заказ оформлен"));
-    }
+        ActionWithOrder.pressOrderNumberWindow();}
     @Test
     public void testOrderScooterDownButton() {
         //Нажимаем кнопку Заказать
-        WebElement orderButtonTop = driver.findElement(By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"));
-        orderButtonTop.click();
-        //Заполняем поля заказа
+        ActionWithOrder.clickButtonOrderDown();
+        // Заполняем поля заказа
         //Имя
-        WebElement nameInput = driver.findElement(By.xpath(".//input[@placeholder='* Имя']"));
-        nameInput.sendKeys("Александр");
+        ActionWithOrder.sendNameInput();
         //Фамилия
-        WebElement surnameInput = driver.findElement(By.xpath(".//input[@placeholder='* Фамилия']"));
-        surnameInput.sendKeys("Парлакян");
+        ActionWithOrder.sendSurnameInput();
         //Адресс доставки
-        WebElement addressInput = driver.findElement(By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']"));
-        addressInput.sendKeys("Ленинградский проспект дом 2 кор.1");
+        ActionWithOrder.sendAddressInput();
         //Телефон для связи с курьером
-        WebElement phoneInput = driver.findElement(By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']"));
-        phoneInput.sendKeys("+79211065265");
+        ActionWithOrder.sendPhoneInput();
         //Выбираем станцию метро Белорусская
-        WebElement stationMetro = driver.findElement(By.xpath(".//input[@placeholder='* Станция метро']"));
-        stationMetro.click();
-        stationMetro.sendKeys("Белорусская");
-        WebElement stationMetroUser = driver.findElement(By.xpath(".//button[@value='29']"));
-        stationMetroUser.click();
+        ActionWithOrder.choiceStationMetro();
         //Нажимаем кнопку далее
-        WebElement orderButtonNext = driver.findElement(By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"));
-        orderButtonNext.click();
+        ActionWithOrder.orderButtonNextPage();
         //Заполняем дату доставки
-        WebElement orderArrivalDate = driver.findElement(By.xpath(".//input[@placeholder='* Когда привезти самокат']"));
-        orderArrivalDate.click();
-        orderArrivalDate.sendKeys("10.10.2025");
-        orderArrivalDate.sendKeys(Keys.ENTER);
-        WebElement orderRentalPeriod = driver.findElement(By.className("Dropdown-control"));
-        orderRentalPeriod.click();
-        WebElement orderRentalPeriodDay = driver.findElement(By.className("Dropdown-option"));
-        orderRentalPeriodDay.click();
+        ActionWithOrder.deliveryDateSelection();
         //Выбираем чек-бокс с цветом самоката
-        WebElement colorRentScooter = driver.findElement(By.id("black"));
-        colorRentScooter.click();
+        ActionWithOrder.choiceColorRentScooter();
         //Заполняем комментарий
-        WebElement commentCourier = driver.findElement(By.xpath(".//input[@placeholder='Комментарий для курьера']"));
-        commentCourier.sendKeys("Надо срочно");
+        ActionWithOrder.sendCommentCourier();
         //Нажимаем кнопку Заказать
-        WebElement makeAnOrder = driver.findElement(By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"));
-        makeAnOrder.click();
+        ActionWithOrder.pressMakeAnOrder();
         //Нажимаем кнопку подтверждения заказа
-        WebElement confirmTheOrder = driver.findElement(By.xpath(".//button[contains(@class, 'Button_Button__ra12g') and text()='Да']"));
-        confirmTheOrder.click();
+        ActionWithOrder.pressConfirmTheOrder();
         //Выполняем сверку окна подтверждения заказа по тексту
-        WebElement orderNumberWindow = driver.findElement(By.cssSelector("div.Order_ModalHeader__3FDaJ"));
-        String orderNumberText = orderNumberWindow.getText();
-        assertEquals("Надпись 'Заказ оформлен' отображается корректно", true, orderNumberText.contains("Заказ оформлен"));
+        ActionWithOrder.pressOrderNumberWindow();
     }
 }
+
--- src/test/java/QuestionsAboutImportantTestsChrome.java
+++ src/test/java/QuestionsAboutImportantTestsChrome.java
@@ -1,10 +1,11 @@
-import baseTest.BaseSetUpChrome;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+/*
 public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
     @Test
     public void testDropdownListHowMuch() {
@@ -12,10 +13,10 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-0"));
+        private final By dropdownArrow = By.id("accordion__heading-0"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//*[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
+        private final By dropDownText = By.xpath("//*[contains(text(), 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue("Надпись 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.' отображается на странице", dropDownTextTest.contains("Сутки — 400 рублей. Оплата курьеру — наличными или картой."));
     }
@@ -24,10 +25,10 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-1"));
+        private final By dropdownArrow = By.id("accordion__heading-1"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//p[contains(text(), 'Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.')]"));
+        private final By dropDownText = By.xpath("//p[contains(text(), 'Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue("Надпись 'Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.' отображается на странице", dropDownTextTest.contains("Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."));
     }
@@ -36,10 +37,10 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-2"));
+        private final By dropdownArrow = By.id("accordion__heading-2"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//*[contains(text(), 'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.')]"));
+        private final By dropDownText = By.xpath("//*[contains(text(), 'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue(dropDownTextTest.contains("Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."));
     }
@@ -48,10 +49,10 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-3"));
+        private final By dropdownArrow = By.id("accordion__heading-3"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//p[contains(text(), 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.')]"));
+        private final By dropDownText = By.xpath("//p[contains(text(), 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue(dropDownTextTest.contains("Только начиная с завтрашнего дня. Но скоро станем расторопнее."));
     }
@@ -60,10 +61,10 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-4"));
+        private final By dropdownArrow = By.id("accordion__heading-4"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//p[contains(text(), 'Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.')]"));
+        private final By dropDownText = By.xpath("//p[contains(text(), 'Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue(dropDownTextTest.contains("Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."));
     }
@@ -72,10 +73,10 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-5"));
+        private final By dropdownArrow = By.id("accordion__heading-5"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//p[contains(text(), 'Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.')]"));
+        private final By dropDownText = By.xpath("//p[contains(text(), 'Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue(dropDownTextTest.contains("Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."));
     }
@@ -84,10 +85,10 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-6"));
+        private final By dropdownArrow = By.id("accordion__heading-6"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//p[contains(text(), 'Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.')]"));
+        private final By dropDownText = By.xpath("//p[contains(text(), 'Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue(dropDownTextTest.contains("Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."));
     }
@@ -96,11 +97,11 @@ public class QuestionsAboutImportantTestsChrome extends BaseSetUpChrome {
         //Скролим вниз страницы
         jsChrome.executeScript("window.scrollTo(0,document.body.scrollHeight)");
         //Нажимаем на всплывающий список
-        WebElement dropdownArrow = driver.findElement(By.id("accordion__heading-7"));
+        private final By dropdownArrow = By.id("accordion__heading-7"));
         dropdownArrow.click();
         //Получаем текст из списка и сверяем по шаблону
-        WebElement dropDownText = driver.findElement(By.xpath("//p[contains(text(), 'Да, обязательно. Всем самокатов! И Москве, и Московской области.')]"));
+        private final By dropDownText = By.xpath("//p[contains(text(), 'Да, обязательно. Всем самокатов! И Москве, и Московской области.')]"));
         String dropDownTextTest = dropDownText.getText();
         assertTrue(dropDownTextTest.contains("Да, обязательно. Всем самокатов! И Москве, и Московской области."));
     }
-}
+}*/
